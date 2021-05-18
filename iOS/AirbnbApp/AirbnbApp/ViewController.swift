685fbfd44560877a753211f615ee578626d12307
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-18 13:55:52 +0900

    Feat: set .gitignore

---
+++ iOS/AirbnbApp/AirbnbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirbnbApp
+//
+//  Created by 김지선 on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
